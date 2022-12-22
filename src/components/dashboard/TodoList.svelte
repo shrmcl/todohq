fe833d9580ff20a0da41fb91abf723741f62691d
Shar McLeod <shr.mcl@gmail.com>
2022-12-21 20:15:39 -0600

    temp solve for input bind. added task removal.

--- src/components/dashboard/TodoList.svelte
+++ src/components/dashboard/TodoList.svelte
@@ -1,15 +1,14 @@
 <script>
-	import { each } from 'svelte/internal';
-
 	export let todos;
+	export let remove;
 </script>
 
 <div class="rounded-lg shadow-lg p-8 flex flex-col gap-4">
-    <!-- svelte version of .map -->
+	<!-- svelte version of .map -->
 	{#each todos as item, index}
 		<div class="rounded-md p-4 flex flex-row justify-between border-2 border-gray-800">
 			<div class="font-medium text-sm">{item.text}</div>
-			<button>❌</button>
+			<button on:click={() => remove(index)}>❌</button>
 		</div>
 	{/each}
 </div>
